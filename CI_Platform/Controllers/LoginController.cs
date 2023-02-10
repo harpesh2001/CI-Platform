d4bde52debd3ad3ad3ab3b34aad0545e261fdeab
Harpesh Patel <harpeshj2001@gmail.com>
2023-02-10 18:27:06 +0530

    CI Platform Login Page

---
+++ CI_Platform/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
